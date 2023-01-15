d4c68b9b33eac5ef6b647fbef81a58cdf2a7a486
Adrienh <matick973@msn.com>
2023-01-15 17:08:03 +0100

    INIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Routes from './components/Routes';
+import { UserIdContext } from './components/AppContext';
+import { AdminContext } from './components/AppContext';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { oneUser } from './feature/user.slice';
+
 
 function App() {
+  
+  const dispatch = useDispatch()
+  const [ uid, setUid ] = useState(null)
+  const [ isAdmin, setIsAdmin ] = useState(false)                 //Active les options d'administrateurs 
+  
+  useEffect(() => {
+
+    const fetchUserIdToken = async () => {
+      
+      await axios({
+        method: 'get', 
+        url: `${process.env.REACT_APP_API_URL}auth`,
+        withCredentials: true,
+      })
+      
+      .then((res) => { /*console.log(res)*/; setUid(res.data);
+      
+        if(uid){
+    
+          axios({
+            method: 'get', 
+            url: `${process.env.REACT_APP_API_URL}api/auth/user/${uid}`,
+            withCredentials: true,
+          })
+            .then((res) => { /*console.log(res);*/ dispatch(oneUser(res.data))
+            
+              sessionStorage.setItem("userID", res.data.User._id);
+              sessionStorage.setItem("pseudo", res.data.User.pseudo);
+            
+            if (res.data.User.admin === true) {
+              setIsAdmin(true)
+            }
+
+            })
+          .catch((err) => console.log(err, "No Token = No User"))
+        
+        } 
+      
+      })
+
+      .catch((err) => console.log(err, "Undefined Token"))
+    }
+
+    fetchUserIdToken()
+
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserIdContext.Provider value={uid}>
+      <AdminContext.Provider value={isAdmin}>
+        <Routes />
+      </AdminContext.Provider>
+    </UserIdContext.Provider>
+
+  )
 }
 
 export default App;
